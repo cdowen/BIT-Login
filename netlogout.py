da9f05906c38e30c3b0b9eeb697b813348054f8a
cdowen <hhylx1950@gmail.com>
2019-11-19 13:56:01 +0800

    Create netlogout.py

---
+++ netlogout.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+                                                                                                                                                                                                                                                                               from selenium import webdriver                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                options = webdriver.ChromeOptions()
+options.add_argument('headless')
+options.binary_location = '/usr/bin/chromium-browser'# chrome path
+browser = webdriver.Chrome(chrome_options=options)
+browser.get('http://10.0.0.55')
+browser.find_element_by_id('username').send_keys('xxxx')# username
+browser.find_element_by_id('password').send_keys('xxxx')# password
+browser.find_element_by_id('pc_logout').click()
+browser.quit()
